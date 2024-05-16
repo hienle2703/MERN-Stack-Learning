baaf38292c43cae73055f2aaf5186d04a7bd53e9
Hien Le <itoutsource25@generali-life.com.vn>
2024-05-16 16:15:34 +0700

    Add ProductDetail Layout

--- project/frontend-app/app/screens/ProductDetails.jsx
+++ project/frontend-app/app/screens/ProductDetails.jsx
@@ -1,9 +1,17 @@
-import { View, Image, StyleSheet, Dimensions } from "react-native";
-import React, { useRef } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Text,
+} from "react-native";
+import React, { useRef, useState } from "react";
 import { Header } from "../components";
 import { useRoute } from "@react-navigation/native";
 import { Carousel } from "react-native-snap-carousel";
 import { colors, defaultStyle } from "../styles/styles";
+import { Avatar } from "react-native-paper";
 
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = SLIDER_WIDTH;
@@ -11,7 +19,7 @@ const ITEM_WIDTH = SLIDER_WIDTH;
 const images = [
   {
     id: "1231239",
-    url: "https://pngimg.com/uploads/macbook/macbook_PNG9.png",
+    url: "https://ipowerresale.com/cdn/shop/products/media_59797707-00a6-453d-9945-e91a14c04c39.png?v=1702539235",
   },
   {
     id: "128939124",
@@ -19,10 +27,28 @@ const images = [
   },
 ];
 
+const name = "Macbook pro";
+const price = 34909;
+const description =
+  "MacBook is a brand of Mac notebook computers designed and marketed by Apple that use Apple's macOS operating system since 2006. The MacBook brand replaced the PowerBook and iBook brands during the Mac transition to Intel processors, announced in 2005. The current lineup consists of the MacBook Air (2008–present) and the MacBook Pro (2006–present). Two different lines simply named `Macbook` existed from 2006 to 2012 and 2015 to 2019. The MacBook brand was the world's top-selling line of premium laptops as of 2015.[1]";
+const stock = 200;
+
 const ProductDetails = () => {
   const route = useRoute();
+  // const { name } = route?.params || {}
 
   const isCarousel = useRef();
+  const [quantity, setQuantity] = useState(1);
+
+  const decreaseQty = () => {
+    if (quantity <= 1) return null;
+    setQuantity((prev) => prev - 1);
+  };
+
+  const increaseQty = () => {
+    if (quantity > stock) return null;
+    setQuantity((prev) => prev + 1);
+  };
 
   const CarouselCardItem = ({ item, index }) => {
     return (
@@ -43,13 +69,94 @@ const ProductDetails = () => {
       >
         <Header back />
         <Carousel
-          layout={"stack"}
+          layout={"default"}
           sliderWidth={SLIDER_WIDTH}
           itemWidth={ITEM_WIDTH}
           ref={isCarousel}
           data={images}
           renderItem={CarouselCardItem}
         />
+
+        <View
+          style={{
+            backgroundColor: colors.color2,
+            padding: 35,
+            flex: 1,
+            marginTop: -380,
+            borderTopLeftRadius: 55,
+            borderTopRightRadius: 55,
+          }}
+        >
+          <Text numberOfLines={2} style={{ fontSize: 25 }}>
+            {name}
+          </Text>
+          <Text style={{ fontSize: 18, fontWeight: "900" }}>${price}</Text>
+          <Text
+            numberOfLines={8}
+            style={{ letterSpacing: 1, lineHeight: 20, marginVertical: 15 }}
+          >
+            {description}
+          </Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingHorizontal: 5,
+            }}
+          >
+            <Text style={{ color: colors.color3, fontWeight: "100" }}>
+              Quantity
+            </Text>
+            <View
+              style={{
+                width: 80,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Pressable onPress={decreaseQty}>
+                <Avatar.Icon
+                  icon={"minus"}
+                  size={20}
+                  style={{
+                    borderRadius: 5,
+                    backgroundColor: colors.color5,
+                    height: 25,
+                    width: 25,
+                  }}
+                />
+              </Pressable>
+              <Text
+                style={{
+                  backgroundColor: colors.color4,
+                  height: 25,
+                  width: 25,
+                  textAlign: "center",
+                  borderRadius: 5,
+                  borderWidth: 1,
+                  borderColor: colors.color5,
+                }}
+              >
+                {quantity}
+              </Text>
+              <Pressable onPress={increaseQty}>
+                <Avatar.Icon
+                  icon={"plus"}
+                  size={20}
+                  style={{
+                    borderRadius: 5,
+                    backgroundColor: colors.color5,
+                    height: 25,
+                    width: 25,
+                  }}
+                />
+              </Pressable>
+            </View>
+          </View>
+        </View>
       </View>
     </>
   );
@@ -57,7 +164,6 @@ const ProductDetails = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
     width: ITEM_WIDTH,
     paddingVertical: 40,
     height: 380,
