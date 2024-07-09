d9222ec90049648fb20d6b5fcaf23a85ca1ad2da
O2F-IT\vutoan.le_amaris <vutoan.le@amaris.com>
2024-07-09 14:06:58 +0700

    Setup Database

--- project/server/app.js
+++ project/server/app.js
@@ -2,7 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 
 config({
-    path: "./data/config.env"
-})
+  path: "./data/config.env",
+});
 
 export const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Import Routers here
+import user from "./routes/user.js";
+app.use("/api/v1/user", user);
--- project/server/server.js
+++ project/server/server.js
@@ -1,7 +1,9 @@
 import { app } from "./app.js";
+import { connectDB } from "./data/database.js";
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
-  console.log(process.env);
   console.log(
     `Server is listening on port ${process.env.PORT}, in ${process.env.NODE_ENV} MODE`
   );
