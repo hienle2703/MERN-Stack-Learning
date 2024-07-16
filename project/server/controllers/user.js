ef49271ab142c5c1b8babd11bf0b553c0996846a
O2F-IT\vutoan.le_amaris <vutoan.le@amaris.com>
2024-07-16 15:50:11 +0700

    Adding error Handler

--- project/server/controllers/user.js
+++ project/server/controllers/user.js
@@ -1,20 +1,19 @@
+import { asyncError } from "../middlewares/error.js";
 import { User } from "../models/user.js";
+import ErrorHandler from "../utils/error.js";
 
 export const getMyProfile = (req, res, next) => {
   res.send("Profile Data!");
 };
 
-export const login = async (req, res, next) => {
+export const login = asyncError(async (req, res, next) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email }).select("+password");
   const isMatch = await user.comparePassword(password);
-  
+
   if (!isMatch) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid email or password",
-    });
+    return next(new ErrorHandler("Incorrect password", 400));
   }
 
   res.status(200).json({
@@ -22,13 +21,13 @@ export const login = async (req, res, next) => {
     message: `Welcome back, ${user.name}!`,
     data: user,
   });
-};
+});
 
-export const signup = (req, res, next) => {
+export const signup = asyncError(async (req, res, next) => {
   const { name, email, password, address, city, country, pinCode } = req.body;
 
   // Add Cloudinary here
-  User.create({
+  await User.create({
     name,
     email,
     password,
@@ -42,4 +41,4 @@ export const signup = (req, res, next) => {
     success: true,
     message: "User created successfully!",
   });
-};
+});
