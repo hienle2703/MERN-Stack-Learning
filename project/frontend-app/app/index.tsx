ececb4bf99be7ed4f822c7adaa466bea0cef6c9e
O2F-IT\vutoan.le_amaris <vutoan.le@amaris.com>
2024-08-02 11:57:35 +0700

    Add libraries for toolkit redux

--- project/frontend-app/app/index.tsx
+++ project/frontend-app/app/index.tsx
@@ -1,12 +1,14 @@
 import { AppRegistry, StatusBar } from "react-native";
 import Main from "./Main";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 const Index = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle={"dark-content"} />
       <Main />
-    </>
+    </Provider>
   );
 };
 
